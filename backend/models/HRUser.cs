98f6d1b469b1c86ae60f9de55ac7377c9b45cebd
Antunes <simao-miguel.antunes@capgemini.com>
2025-12-11 10:06:36 +0000

    Added the API's base models

---
+++ backend/models/HRUser.cs
@@ -0,0 +1,13 @@
+public class HRUser
+{
+    public int HRUserId { get; set; }
+    public int EmployeeId { get; set; }
+    public string Role { get; set; } = "HR";
+
+    public bool CanEditProfiles { get; set; } = true;
+    public bool CanManagePayments { get; set; } = true;
+    public bool CanAccessResumes { get; set; } = true;
+    public bool CanApproveCandidates { get; set; } = true;
+
+    public Employee? Employee { get; set; }
+}
---
+++ backend/models/PayHistory.cs
@@ -0,0 +1,10 @@
+public class PayHistory
+{
+    public int PayHistoryId { get; set; }
+    public int EmployeeId { get; set; }
+    public DateTime RateChangeDate { get; set; }
+    public decimal Rate { get; set; }
+    public int PayFrequency { get; set; }
+
+    //public Employee Employee { get; set; }
+}
