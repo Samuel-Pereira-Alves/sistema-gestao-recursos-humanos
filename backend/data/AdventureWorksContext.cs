ebd52ec38759f131dff27ef5f4f3ac91d5b9327f
Pacheco <tiago-alexandre.pacheco@capgemini.com>
2025-12-11 19:31:44 +0000

    Added the missing relationships

--- backend/data/AdventureWorksContext.cs
+++ backend/data/AdventureWorksContext.cs
@@ -18,11 +18,29 @@ namespace sistema_gestao_recursos_humanos.backend.data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Employee>()
+                .ToTable("Employee", "HumanResources")
+                .HasKey(e => e.BusinessEntityID);
+
             modelBuilder.Entity<PayHistory>()
+                .ToTable("EmployeePayHistory", "HumanResources")
                 .HasKey(ph => new { ph.BusinessEntityID, ph.RateChangeDate });
 
+            modelBuilder.Entity<PayHistory>()
+                .HasOne(ph => ph.Employee)
+                .WithMany(e => e.PayHistories)
+                .HasForeignKey(ph => ph.BusinessEntityID);
+
+            // DepartmentHistory
             modelBuilder.Entity<DepartmentHistory>()
+                .ToTable("EmployeeDepartmentHistory", "HumanResources")
                 .HasKey(dh => new { dh.BusinessEntityID, dh.DepartmentID, dh.ShiftID, dh.StartDate });
+
+            modelBuilder.Entity<DepartmentHistory>()
+                .HasOne<Employee>()
+                .WithMany(e => e.DepartmentHistories)
+                .HasForeignKey(dh => dh.BusinessEntityID);
         }
     }
 }
