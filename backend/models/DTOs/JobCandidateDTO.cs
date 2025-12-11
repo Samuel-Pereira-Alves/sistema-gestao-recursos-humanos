674efc4da600fa48677c2328d1c71356f6285552
Antunes <simao-miguel.antunes@capgemini.com>
2025-12-11 10:33:41 +0000

    Added the API's base DTO's

---
+++ backend/models/DTOs/JobCandidateDTO.cs
@@ -0,0 +1,6 @@
+public class JobCandidateDto
+{
+    public int JobCandidateId { get; set; }
+    public string Resume { get; set; } = string.Empty;
+    public DateTime ModifiedDate { get; set; }
+}
---
+++ backend/models/DTOs/PayHistoryDTO.cs
@@ -0,0 +1,6 @@
+public class PayHistoryDto
+{
+    public DateTime RateChangeDate { get; set; }
+    public decimal Rate { get; set; }
+    public int PayFrequency { get; set; }
+}
