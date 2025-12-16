09f37ae3888e511d3fa10659dfab97b433a61b7f
Antunes <simao-miguel.antunes@capgemini.com>
2025-12-16 10:09:07 +0000

    Added notifications

---
+++ backend/controllers/NotificationController.cs
@@ -0,0 +1,76 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
+using sistema_gestao_recursos_humanos.backend.data;
+using sistema_gestao_recursos_humanos.backend.models;
+using sistema_gestao_recursos_humanos.backend.models.dtos;
+using sistema_gestao_recursos_humanos.backend.models.tools;
+
+namespace sistema_gestao_recursos_humanos.backend.controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class NotificationController : ControllerBase
+    {
+        private readonly AdventureWorksContext _db;
+        private readonly IMapper _mapper;
+        private readonly IWebHostEnvironment _env;
+
+        public NotificationController(AdventureWorksContext db, IMapper mapper, IWebHostEnvironment env)
+        {
+            _db = db;
+            _mapper = mapper;
+            _env = env;
+        }
+
+        // GET: api/v1/notification
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var notifications = await _db.Notifications.ToListAsync();
+            var dto = _mapper.Map<List<NotificationDto>>(notifications);
+            return Ok(dto);
+        }
+
+        // GET: api/v1/notification/{id}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var notification = await _db.Notifications
+                .FirstOrDefaultAsync(notif => notif.ID == id);
+
+            if (notification == null) return NotFound();
+
+            var dto = _mapper.Map<NotificationDto>(notification);
+            return Ok(dto);
+        }
+
+        // POST: api/v1/notification
+        [HttpPost]
+        public async Task<IActionResult> Create(NotificationDto dto)
+        {
+            var notification = _mapper.Map<Notification>(dto);
+
+            _db.Notifications.Add(notification);
+            await _db.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Get),
+                new { id = notification.ID },
+                _mapper.Map<NotificationDto>(notification));
+        }
+
+        // DELETE: api/v1/notification/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var notification = await _db.Notifications.FindAsync(id);
+            if (notification == null) return NotFound();
+
+            _db.Notifications.Remove(notification);
+            await _db.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ backend/models/Notification.cs
@@ -0,0 +1,15 @@
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace sistema_gestao_recursos_humanos.backend.models
+{
+    [Table("AppMessages", Schema = "HumanResources")]
+    public class Notification
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Message { get; set; } = string.Empty;
+    }
+}
