4942dbdce40c61905246ecfca268dc8076694c3c
Alves <samuel-pereira.alves@capgemini.com>
2026-01-15 14:37:24 +0000

    Refactoring

--- frontend/src/Service/loginService.jsx
+++ frontend/src/Service/loginService.jsx
@@ -2,10 +2,29 @@ const API_BASE = "http://localhost:5136/api/v1";
 
 export async function login(username, password) {
   const res = await fetch(`${API_BASE}/login`, {
-    headers: { "Content-Type": "application/json"},
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
     body: JSON.stringify({ username, password }),
-    method: "POST"
   });
-  if (!res.ok) throw new Error("Erro ao carregar departamentos");
-  return res;
+
+  if (!res.ok) {
+    let msg = `Falha no login (HTTP ${res.status})`;
+    try {
+      const ct = res.headers.get("content-type") || "";
+      if (ct.includes("application/json")) {
+        const err = await res.json();
+        if (err?.message) msg = err.message;
+        else msg = `${msg} - ${JSON.stringify(err)}`;
+      } else {
+        const txt = await res.text();
+        if (txt) msg = `${msg} - ${txt}`;
+      }
+    } catch { }
+    throw new Error(msg);
+  }
+
+  return res.json(); 
 }
